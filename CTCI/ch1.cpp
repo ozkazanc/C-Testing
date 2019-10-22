a406d8069578746fb5f95133ea6056bd12d58829
ozkazanc <doruk.ozkazanc@mail.utoronto.ca>
2019-10-22 16:00:36 -0400

    Started chapter 1

---
+++ CTCI/ch1.cpp
@@ -0,0 +1,31 @@
+#include "ch1.h"
+
+namespace CTCI {
+
+	
+
+
+	void Main() {
+
+	}
+
+	bool IsUnique(const std::string& str) {
+
+	}
+
+	bool CheckPermutation(const std::string& a, const std::string& b) {
+
+	}
+
+	std::string& URLify(std::string& src, int length) {
+
+	}
+
+	bool PermutationPalindrome(const std::string& src) {
+
+	}
+
+	bool OneAway(const std::string& a, const std::string& b) {
+
+	}
+}
---
+++ CTCI/ch1.h
@@ -0,0 +1,27 @@
+#pragma once
+#include <string>
+
+
+// Problems from Chapter 1 of Cracking the Coding Interview, 6th Edition
+namespace CTCI {
+
+	// Given a string, determine if it has all unique characters
+	bool IsUnique(const std::string& str);
+	
+	// Given 2 strings check is they are permuatations of one another
+	bool CheckPermutation(const std::string& a, const std::string& b);
+
+	// Given a string and its true lenghtr replace all spaces(' ') with "%20",
+	// assume you have enough space at the end of string as buffer
+	std::string& URLify(std::string& src, int lenght);
+
+	// Given a string, determine if the string is a permutation of a palindrome
+	bool PermutationPalindrome(const std::string& src);
+
+	// Given 2 string, determine if the number of edits you'd need to make to one of them
+	// to turn into the other one is less than 1.
+	bool OneAway(const std::string& a, const std::string& b);
+
+
+	void Main();
+}
