433b540338a6c3fd78c8f3ca1587745df159f58a
ozkazanc <doruk.ozkazanc@mail.utoronto.ca>
2019-10-24 16:08:43 -0400

    2 more q's done

--- CTCI/ch1.cpp
+++ CTCI/ch1.cpp
@@ -1,6 +1,5 @@
 #include "ch1.h"
 #include <iostream>
-#include <vector>
 #include <unordered_map>
 #include "LevenshteinDistance.h"
 namespace CTCI {
@@ -34,6 +33,23 @@ namespace CTCI {
 		}
 		std::cout << std::endl;
 	// Q6 ================================================================================================
+		std::vector<std::string> compression{ "abbccccccca", "heeeello", "never odd or even", "odd", "yeshhees", "eeeyes", "ssss" };
+		for (const auto& elem : compression)
+			std::cout << elem << " turned into: " << StringCompression(elem) << '\n';
+		std::cout << std::endl;
+	// Q7 ================================================================================================
+		std::vector<std::vector<int>> mat90{{1 , 2, 3, 4, 5},
+											{6 , 7, 8, 9,10},
+											{11,12,13,14,15},
+											{16,17,18,19,20},
+											{21,22,23,24,25} };
+		PrintMatrix(mat90);
+		RotateMatrix90Degrees(mat90);
+		std::cout << std::endl;
+		PrintMatrix(mat90);
+		std::cout << std::endl;
+	// Q8 ================================================================================================
+	// Q9 ================================================================================================
 
 	}
 
@@ -107,6 +123,10 @@ namespace CTCI {
 	}
 
 	bool PermutationPalindrome(const std::string& src) {
+		
+		// ============== Hash Table Solution ===================================================
+		/*
+
 		// We do not wanr to take spaces into account
 		std::string temp = EliminateSpaces(src);
 
@@ -129,8 +149,38 @@ namespace CTCI {
 		// Can be done using a bitset to toggle bits, 1 if odd, 0 if even and check if there is only 1 bit set.
 		// O(n) time, where n is the length of the string
 		// O(n) space, same as time analysis
-	}
 
+		*/
+
+		// ============= Bitset solution by toggling ===============================================
+
+		int32_t bitflag = 0;
+
+		for (size_t i = 0; i < src.length(); i++) {
+			// Don't count space characters
+			if (std::isspace(src[i])) continue;
+			int offset = src[i] - 'a';
+
+			// Toggle the corresponding bit
+			bitflag ^= 1 << offset;
+		}
+		// If all characters in the string are even numbered bitflag will be 0,
+		// If only one character is odd numbered (bitflag & (bitflag - 1)) will be 0.
+		// ex.
+		// bitflag					=  00100000
+		// bitflag - 1				=  00011111
+		// bitflag & (bitflag - 1)	=  00000000 iff there is exactly 1 bit set. 
+
+		// Think of it like getting rid of a digit. The only way to get rid of a digit 
+		// from a number which has n digits is by substracting 1 
+		// is if the original number is the smallest number which has n digits.
+		// ex.
+		// 100 (3 digits) - 1 = 99 (2 digits)
+		return bitflag == 0 || (bitflag & (bitflag - 1)) == 0;
+
+		// O(n) time, where n is the length of the string
+		// O(2^n) space, where n is the number of bits used to store a character.
+	}
 
 	bool OneAway(const std::string& a, const std::string& b) {
 		return LevenshteinDistance::CalculateLevenshtein(a, b) < 2;
@@ -138,5 +188,70 @@ namespace CTCI {
 		//Levenshtein distance is an algorithm that calculates the number of edits between 2 strings
 		// O(n * m) time, where n and m are the respective lenght of the strings
 		// O(n * m) space, same as time analysis
+	}
+
+	std::string StringCompression(const std::string& src) {
+		std::vector<char> comp;
+
+		// Keep track of the last seen character and count how many times it was seen
+		char lastSeen = src[0];
+		int count = 0;
+
+		for (size_t i = 0; i < src.length(); i++) {
+			if (lastSeen == src[i]) count++;
+			else {
+				comp.push_back(lastSeen);
+				comp.push_back(char('0' + count));
+
+				// Change the last seen character, count is already one since itself counts as the first
+				lastSeen = src[i];
+				count = 1;
+			}
+		}
+		// Do not forget to push back the last element
+		comp.push_back(lastSeen);
+		comp.push_back(char('0' + count));
+
+		std::string res(comp.begin(), comp.end());
+		return src.length() > res.length() ? res : src;
+
+		// O(n) time, where n is the length of the string
+		// O(n) space, same as time analysis
+	}
+	
+	void PrintMatrix(const std::vector<std::vector<int>>& mat) {
+		for (const auto& row : mat) {
+			for (const auto& col : row)
+				std::cout << col;
+			std::cout << '\n';
+		}
+	}
+
+	void RotateMatrix90Degrees(std::vector<std::vector<int>>& mat){
+		unsigned int N = mat.size();
+		// Rotate the inner layers
+		for (unsigned int x = 0; x < N / 2; x++) {
+			for (unsigned int y = x; y < N - 1 - x; y++) {
+
+				// Rotate in 4's
+				int temp = mat[x][y];
+				mat[x][y] = mat[y][N - 1 - x];
+				mat[y][N - 1 - x] = mat[N - 1 - x][N - 1 - y];
+				mat[N - 1 - x][N - 1 - y] = mat[N - 1 - y][x];
+				mat[N - 1 - y][x] = temp;
+			}
+		}
+		return;
+
+		// O(n^2) time, where n is the size of the matrix
+		// O(1) space, no additional space is neededs
+	}
+
+	void ZeroMatrix(std::vector<std::vector<int>>& matrix){
+	
+	}
+
+	bool StringRotation(const std::string& a, const std::string& b){
+		return false;
 	}
 }
--- CTCI/ch1.h
+++ CTCI/ch1.h
@@ -1,6 +1,6 @@
 #pragma once
 #include <string>
-
+#include <vector>
 
 // Problems from Chapter 1 of Cracking the Coding Interview, 6th Edition
 namespace CTCI {
@@ -22,6 +22,22 @@ namespace CTCI {
 	// to turn into the other one is less than 1.
 	bool OneAway(const std::string& a, const std::string& b);
 
+	// Given a string of characters from 'a' .. 'z', compress it by the number of occurrences of characters
+	// Return the original string if the length of the compressed string is higher than the length og the original string.
+	// ex. "aabccca" -> "a2b1c3a1"
+	std::string StringCompression(const std::string& src);
+
+
+	void PrintMatrix(const std::vector<std::vector<int>>& mat);
+	// Rotate a matrix by 90 in place
+	void RotateMatrix90Degrees(std::vector<std::vector<int>>& matrix);
+
+	// If an element in the given matrix is 0, set the entire row and column to also 0.
+	void ZeroMatrix(std::vector<std::vector<int>>& matrix);
+
+	// By only calling a function isSubstring(), determine if the 2 given strings are rotations of one another
+	// ex. waterbottle and terbottlewa are rotations
+	bool StringRotation(const std::string& a, const std::string& b);
 
 	void Ch1_Main();
 }
