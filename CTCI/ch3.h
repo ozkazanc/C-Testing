488028731729301a1888c3d6db53cdba17ebb07c
ozkazanc <doruk.ozkazanc@mail.utoronto.ca>
2019-11-04 16:25:52 -0500

    Set up ch3

---
+++ CTCI/ch3.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#include "../Core.h"
+
+// Stacks and Queues
+// Problems from Chapter 3 of Cracking the Coding Interview, 6th Edition
+namespace CTCI {
+
+	// Single array to implement 2 stacks
+
+	// A stack that has a O(1) min function that returns the minimum element
+
+	// Stack of plates where if the number of elements go over a MAX_THRESHOLD
+	// push the nex element into a new stack
+
+	// Implement a queue with 2 stacks
+
+	// Sort a stack by only using 1 more stack
+
+	// Animal Shelter, create a data structures to maintain a system where
+	// People can only adopt the oldest cat, dog or animal(cats and dogs).
+	// They can choose the kind of animal they want but they cannot specifically
+	// pick an element (i.e. 3 queues)
+
+	void Ch3_Main();
+}
