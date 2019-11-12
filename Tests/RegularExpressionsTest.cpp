96ff66eba6f1a88dc603d91fd9827d5a96e17ab7
ozkazanc <doruk.ozkazanc@mail.utoronto.ca>
2019-11-12 09:48:18 -0500

    Implemented capturing info from a match

--- Tests/RegularExpressionsTest.cpp
+++ Tests/RegularExpressionsTest.cpp
@@ -13,13 +13,13 @@ It's very useful in designing a compiler, and user input checking and what not. 
 		//std::regex re("\\sc[a-z\\+]*");
 		//std::string ans("c++, compiler, checking");
 
-		// All words ending with 'ing'
-		//std::regex re("[a-z]*ing\\s");
-		//std::string ans("string, testing, designing, checking");
+		// All words ending with 'ing' with capturing additional info
+		std::regex re("([a-z]*)(ing)\\s");
+		std::string ans("string, testing, designing, checking");
 
 		// All 2 letter words
-		std::regex re("\\s[a-zA-Z]{2}[\\s!\)]");
-		std::string ans("is, to, in, in, to, it");
+		//std::regex re("\\s[a-zA-Z]{2}[\\s!\)]");
+		//std::string ans("is, to, in, in, to, it");
 
 		auto matches_begin = std::sregex_iterator(str.begin(), str.end(), re);
 		auto matches_end = std::sregex_iterator();
@@ -30,8 +30,12 @@ It's very useful in designing a compiler, and user input checking and what not. 
 
 		for (std::sregex_iterator i = matches_begin; i != matches_end; i++) {
 			std::smatch m = *i;
+			
+			std::cout << "Captured ";
+			for (std::size_t i = 0; i < m.size(); i++)	 std::cout << i << "-" << m[i] << " ";
+			
 			std::string match_str = m.str();
-			std::cout << match_str << '\n';
+			std::cout << "\nMatch "<< match_str << "\n\n";
 		}
 
 		std::cout << "Result should be " << ans << std::endl;
